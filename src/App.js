a88bd3c812bb0043c868af5431de43832ccd42b2
HsnAkk <hasan.akkas@web.de>
2019-10-14 13:26:34 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent'; 
 import './App.css';
 
@@ -7,9 +8,12 @@ class App extends Component {
   
   render () {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
+      
     );
   }
 }
